321f1d38923efe2ff6711122e19cf354002408bf
Göth Radim <radim.goeth@ysoft.com>
2023-04-03 14:41:19 +0200

    uml

--- CSharp/UnicodeFileToHtmlTextConverter/UnicodeFileToHtmlTextConverter.cs
+++ CSharp/UnicodeFileToHtmlTextConverter/UnicodeFileToHtmlTextConverter.cs
@@ -34,16 +34,4 @@ namespace TDDMicroExercises.UnicodeFileToHtmlTextConverter
             }
         }
     }
-    class HttpUtility
-    {
-        public static string HtmlEncode(string line)
-        {
-            line = line.Replace("<", "&lt;");
-            line = line.Replace(">", "&gt;");
-            line = line.Replace("&", "&amp;");
-            line = line.Replace("\"", "&quot;");
-            line = line.Replace("\'", "&quot;");
-            return line;
-        }
-    }
 }
